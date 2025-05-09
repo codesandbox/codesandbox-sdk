a718ebdd3d6fcf975aa13e958c523e0c53560886
Christian Alfoni <christianalfoni@gmail.com>
2025-05-09 14:27:02 +0200

    awesome tasks

--- src/sessions/WebSocketSession/tasks.ts
+++ src/sessions/WebSocketSession/tasks.ts
@@ -1,11 +1,11 @@
 import {
   Emitter,
-  IDisposable,
   type IPitcherClient,
   type protocol,
 } from "@codesandbox/pitcher-client";
 
 import { Disposable } from "../../utils/disposable";
+import { DEFAULT_SHELL_SIZE } from "./terminals";
 
 export type TaskDefinition = {
   name: string;
@@ -17,144 +17,149 @@ export type TaskDefinition = {
   };
 };
 
-export type Task = TaskDefinition & {
-  id: string;
-  unconfigured?: boolean;
-  shellId: null | string;
-  ports: protocol.port.Port[];
-};
-
-type TaskShellSubscription = {
-  shellId: string;
-  disposable: IDisposable;
-};
-
 export class Tasks {
   private disposable = new Disposable();
-  private shellOutputListeners: Record<string, TaskShellSubscription> = {};
-  private onTaskOutputEmitter = this.disposable.addDisposable(
-    new Emitter<{
-      taskId: string;
-      output: string;
-    }>()
-  );
-  public readonly onTaskOutput = this.onTaskOutputEmitter.event;
-
+  private tasks: Task[] = [];
   constructor(
     sessionDisposable: Disposable,
     private pitcherClient: IPitcherClient
   ) {
+    this.tasks = Object.values(
+      this.pitcherClient.clients.task.getTasks().tasks
+    ).map((task) => new Task(this.pitcherClient, task));
+
     sessionDisposable.onWillDispose(() => {
       this.disposable.dispose();
     });
-    this.disposable.addDisposable(
-      pitcherClient.clients.task.onTaskUpdate((task) =>
-        this.listenToShellOutput(task)
-      )
-    );
-    this.disposable.onWillDispose(() => {
-      Object.values(this.shellOutputListeners).forEach((listener) =>
-        listener.disposable.dispose()
-      );
-    });
-  }
-
-  private async listenToShellOutput(task: protocol.task.TaskDTO) {
-    const existingListener = this.shellOutputListeners[task.id];
-
-    // Already have shell registered
-    if (existingListener && task.shell?.shellId === existingListener.shellId) {
-      return;
-    }
-
-    // Has removed shell
-    if (
-      existingListener &&
-      (!task.shell || task.shell.shellId !== existingListener.shellId)
-    ) {
-      existingListener.disposable.dispose();
-    }
-
-    // No new shell
-    if (!task.shell) {
-      return;
-    }
-
-    // Has new shell
-    const taskShellId = task.shell.shellId;
-    let listener: TaskShellSubscription = {
-      shellId: taskShellId,
-      disposable: this.pitcherClient.clients.shell.onShellOut(
-        ({ shellId, out }) => {
-          if (shellId === taskShellId) {
-            this.onTaskOutputEmitter.fire({
-              taskId: task.id,
-              output: out,
-            });
-          }
-        }
-      ),
-    };
-
-    this.shellOutputListeners[task.id] = listener;
-
-    this.pitcherClient.clients.shell.open(task.shell.shellId, {
-      cols: 80,
-      rows: 24,
-    });
-
-    /*
-    this.onTaskOutputEmitter.fire({
-      taskId: task.id,
-      output: shell.buffer.join("\n"),
-    });
-    */
   }
 
   /**
    * Gets all tasks that are available in the current sandbox.
    */
-  async getTasks(): Promise<Task[]> {
-    const tasks = await this.pitcherClient.clients.task.getTasks();
-
-    return Object.values(tasks.tasks).map(taskFromDTO);
+  getTasks(): Task[] {
+    return this.tasks;
   }
 
   /**
    * Gets a task by its ID.
    */
-  async getTask(taskId: string): Promise<Task | undefined> {
-    const task = await this.pitcherClient.clients.task.getTask(taskId);
-
-    if (!task) {
-      return undefined;
-    }
-
-    return taskFromDTO(task);
-  }
-
-  /**
-   * Runs a task by its ID.
-   */
-  async runTask(taskId: string): Promise<Task> {
-    const task = await this.pitcherClient.clients.task.runTask(taskId);
-
-    return taskFromDTO(task);
-  }
-
-  async stopTask(taskId: string) {
-    await this.pitcherClient.clients.task.stopTask(taskId);
+  getTask(taskId: string): Task | undefined {
+    return this.tasks.find((task) => task.id === taskId);
   }
 }
 
-function taskFromDTO(value: protocol.task.TaskDTO): Task {
-  return {
-    id: value.id,
-    name: value.name,
-    command: value.command,
-    runAtStart: value.runAtStart,
-    preview: value.preview,
-    shellId: value.shell?.shellId ?? null,
-    ports: value.ports,
+export class Task {
+  private disposable = new Disposable();
+  private get shell() {
+    return this.data.shell;
+  }
+  private openedShell?: {
+    shellId: string;
+    output: string[];
+    dimensions: typeof DEFAULT_SHELL_SIZE;
   };
+  private onOutputEmitter = this.disposable.addDisposable(
+    new Emitter<string>()
+  );
+  public readonly onOutput = this.onOutputEmitter.event;
+  private onStatusChangeEmitter = this.disposable.addDisposable(
+    new Emitter<protocol.shell.ShellProcessStatus | "IDLE">()
+  );
+  public readonly onStatusChange = this.onStatusChangeEmitter.event;
+  get id() {
+    return this.data.id;
+  }
+  get name() {
+    return this.data.name;
+  }
+  get command() {
+    return this.data.command;
+  }
+  get runAtStart() {
+    return this.data.runAtStart;
+  }
+  get ports() {
+    return this.data.ports;
+  }
+  get status() {
+    return this.shell?.status || "IDLE";
+  }
+  constructor(
+    private pitcherClient: IPitcherClient,
+    private data: protocol.task.TaskDTO
+  ) {
+    pitcherClient.clients.task.onTaskUpdate(async (task) => {
+      if (task.id !== this.id) {
+        return;
+      }
+
+      const lastStatus = this.status;
+      const lastShellId = this.shell?.shellId;
+
+      this.data = task;
+
+      if (lastStatus !== this.status) {
+        this.onStatusChangeEmitter.fire(this.status);
+      }
+
+      if (
+        this.openedShell &&
+        task.shell &&
+        task.shell.shellId !== lastShellId
+      ) {
+        const openedShell = await this.pitcherClient.clients.shell.open(
+          task.shell.shellId,
+          this.openedShell.dimensions
+        );
+
+        this.openedShell = {
+          shellId: openedShell.shellId,
+          output: openedShell.buffer,
+          dimensions: this.openedShell.dimensions,
+        };
+
+        this.onOutputEmitter.fire("\x1B[2J\x1B[3J\x1B[1;1H");
+        openedShell.buffer.forEach((out) => this.onOutputEmitter.fire(out));
+      }
+    });
+
+    pitcherClient.clients.shell.onShellOut(({ shellId, out }) => {
+      if (!this.shell || this.shell.shellId !== shellId || !this.openedShell) {
+        return;
+      }
+
+      // Update output for shell
+      this.openedShell.output.push(out);
+      this.onOutputEmitter.fire(out);
+    });
+  }
+  async open(dimensions = DEFAULT_SHELL_SIZE) {
+    if (!this.shell) {
+      throw new Error("Task is not running");
+    }
+
+    const openedShell = await this.pitcherClient.clients.shell.open(
+      this.shell.shellId,
+      dimensions
+    );
+
+    this.openedShell = {
+      shellId: openedShell.shellId,
+      output: openedShell.buffer,
+      dimensions,
+    };
+
+    return this.openedShell.output.join("\n");
+  }
+  async run() {
+    await this.pitcherClient.clients.task.runTask(this.id);
+  }
+  async restart() {
+    await this.run();
+  }
+  async stop() {
+    if (this.shell) {
+      await this.pitcherClient.clients.task.stopTask(this.id);
+    }
+  }
 }
