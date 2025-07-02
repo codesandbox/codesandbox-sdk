75f56dafb1fe93ad395d9727ebab39425875de5d
tristandubbeld <tristan.dubbeld@gmail.com>
2025-07-02 09:35:52 +0200

    fix: add apiClient to context and pass to query fn

--- src/bin/ui/sdkContext.tsx
+++ src/bin/ui/sdkContext.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 import { createContext, useContext } from "react";
 import { CodeSandbox } from "@codesandbox/sdk";
+import { createApiClient } from "../../utils/api";
+import { Client } from "@hey-api/client-fetch";
+import { getInferredApiKey } from "../../utils/constants";
+import { instrumentedFetch } from "../utils/sentry";
 
 const sdk = new CodeSandbox();
 
-export const SDKContext = createContext<CodeSandbox>(sdk);
+const apiKey = getInferredApiKey();
+const apiClient: Client = createApiClient(apiKey, {}, instrumentedFetch);
+
+export const SDKContext = createContext<{ sdk: CodeSandbox; apiClient: Client }>({
+  sdk,
+  apiClient,
+});
+
 
 export const SDKProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SDKContext.Provider value={sdk}>{children}</SDKContext.Provider>;
+  return (
+    <SDKContext.Provider value={{ sdk, apiClient }}>
+      {children}
+    </SDKContext.Provider>
+  );
 };
 
 export function useSDK() {
