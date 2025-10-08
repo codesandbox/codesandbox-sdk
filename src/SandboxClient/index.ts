199ec3f8d2cdb4663ea3a29d66e58f6c657a5c35
Christian Alfoni <christianalfoni@gmail.com>
2025-10-08 14:03:23 +0200

    basic exec run and list

--- src/SandboxClient/index.ts
+++ src/SandboxClient/index.ts
@@ -16,6 +16,7 @@ import { Barrier } from "../utils/barrier";
 import { AgentClient } from "../AgentClient";
 import { SandboxSession } from "../types";
 import { Tracer, SpanStatusCode } from "@opentelemetry/api";
+import { PintClient } from "../PintClient";
 
 export * from "./filesystem";
 export * from "./ports";
@@ -41,6 +42,20 @@ export class SandboxClient {
     initStatusCb?: (event: system.InitStatus) => void,
     tracer?: Tracer
   ) {
+    if (session.isPint) {
+      const agentClient = await PintClient.create(session);
+
+      return new SandboxClient(
+        agentClient,
+        { hostToken: session.hostToken, tracer },
+        {
+          currentStepIndex: 0,
+          state: "FINISHED",
+          steps: [],
+        }
+      );
+    }
+
     const { client: agentClient, joinResult } = await AgentClient.create({
       session,
       getSession,
